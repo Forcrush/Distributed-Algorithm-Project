314a74a692ce547b32417c043245704743681dad
Puffrora <33590171+Forcrush@users.noreply.github.com>
2020-05-18 17:39:10 +0800

    ver2.0

---
+++ Ver2.0/README.md
@@ -0,0 +1,9 @@
+## Raft concensus algorithm demo
+
+### Execution
+
+- cd `demo`
+- run `node1.py`, `node2.py`, `node3.py` respectively (in 3 terminal window)
+- run `client.py` to send msg to the cluster above (in an extra terminal window)
+
+- Based on the code of [hangsz](https://github.com/hangsz/raft)
---
+++ Ver2.0/node2.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+from raft.node import Node
+
+if __name__ == '__main__':
+
+    conf = {'id': 'node_2',
+              'addr': ('localhost', 10002),
+              'peers': { 'node_1': ('localhost', 10001), 
+                         'node_3': ('localhost', 10003)
+                       }
+            }
+     
+    node = Node(conf)
+
+    node.run()
